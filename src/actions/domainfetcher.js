b46674765dc4747a39393afcec32376a0a6c159b
Fraser Crichton <fraser.crichton.developer@gmail.com>
2021-02-05 12:02:49 +1300

    More error handling

--- src/actions/domainfetcher.js
+++ src/actions/domainfetcher.js
@@ -1,69 +1,87 @@
-import serverRequestConfiguration from './serverRequestConfiguration'
-import { httpResourceHelper } from './httpResourceHelper'
-
+import serverRequestConfiguration from "./serverRequestConfiguration";
+import { httpResourceHelper } from "./httpResourceHelper";
 // this needs to deal with missing features so validate against the supported features
 // const supportedFeatures = ['USE_EVENTS', 'USE_ASSOCIATIONS', 'USE_SOURCES', 'USE_SITES', 'USE_SHAPES']
 
-const requestErrorMessage = (url, domain, error) => `The '${domain}' returned ${error.message}`
+const requestErrorMessage = (url, domain, error) =>
+  `The '${domain}' returned ${error.message}`;
 
 const fetchDomain = (features) => {
-  const { USE_ASSOCIATIONS, USE_SOURCES, USE_SITES, USE_SHAPES } = features
+  const { USE_ASSOCIATIONS, USE_SOURCES, USE_SITES, USE_SHAPES } = features;
 
-  const activeFeatures = ['USE_EVENTS', 'USE_SOURCES', 'USE_ASSOCIATIONS', 'USE_SITES', 'USE_SHAPES']
+  const activeFeatures = [
+    "USE_EVENTS",
+    "USE_SOURCES",
+    "USE_ASSOCIATIONS",
+    "USE_SITES",
+    "USE_SHAPES",
+  ];
 
   // test this
   // const { configuration, errors } = serverRequestConfiguration('NO_ARRAY')
-  const { configuration, errors } = serverRequestConfiguration(activeFeatures)
-  const { SOURCES_URL, ASSOCIATIONS_URL, SHAPES_URL, SITES_URL } = configuration
+  const { configuration, errors } = serverRequestConfiguration(activeFeatures);
+  const {
+    SOURCES_URL,
+    ASSOCIATIONS_URL,
+    SHAPES_URL,
+    SITES_URL,
+  } = configuration;
 
-  const notifications = [...errors]
+  const notifications = [...errors];
   // if there are errors don't go any further
   const handleError = (message) => {
     notifications.push({
       message,
-      type: 'error'
-    })
-  }
+      type: "error",
+    });
+  };
 
-  const fetchDomainHttpResource = (url, domain) => httpResourceHelper(url, (error) => handleError(requestErrorMessage(url, domain, error)), [])
+  const fetchDomainHttpResource = (url, domain) =>
+    httpResourceHelper(
+      url,
+      (error) => handleError(requestErrorMessage(url, domain, error)),
+      []
+    );
 
   return () => {
-    // dispatch(toggleFetchingDomain())
+    const EVENT_DATA_URL = [
+      "http://localhost:4040/api/curfew/export_events/deeprows",
+    ];
 
-    const EVENT_DATA_URL = ['http://localhost:4040/api/curfew/export_events/deeprows']
-
-    // NB: EVENT_DATA_URL is a list, and so results are aggregated
+    // everything should be a list
+    // events are mandatory?
     const eventsPromise = Promise.all(
-      EVENT_DATA_URL.map((url) =>
-        fetchDomainHttpResource(url, 'EVENTS_URL')
-      )
-    ).then((results) => results.flatMap((event) => event))
+      EVENT_DATA_URL.map((url) => fetchDomainHttpResource(url, "EVENTS_URL"))
+    ).then((results) => results.flatMap((event) => event));
 
-    let associationsPromise = Promise.resolve([])
+    let associationsPromise = Promise.resolve([]);
     if (USE_ASSOCIATIONS) {
       if (ASSOCIATIONS_URL) {
-        associationsPromise = fetchDomainHttpResource(ASSOCIATIONS_URL, 'ASSOCIATIONS_URL')
+        associationsPromise = fetchDomainHttpResource(
+          ASSOCIATIONS_URL,
+          "ASSOCIATIONS_URL"
+        );
       }
     }
 
-    let sourcesPromise = Promise.resolve([])
+    let sourcesPromise = Promise.resolve([]);
     if (USE_SOURCES) {
       if (SOURCES_URL) {
-        sourcesPromise = fetchDomainHttpResource(SOURCES_URL, 'SOURCES_URL')
+        sourcesPromise = fetchDomainHttpResource(SOURCES_URL, "SOURCES_URL");
       }
     }
 
-    let sitesPromise = Promise.resolve([])
+    let sitesPromise = Promise.resolve([]);
     if (USE_SITES) {
       if (SITES_URL) {
-        sitesPromise = fetchDomainHttpResource(SITES_URL, 'SITES_URL')
+        sitesPromise = fetchDomainHttpResource(SITES_URL, "SITES_URL");
       }
     }
 
-    let shapesPromise = Promise.resolve([])
+    let shapesPromise = Promise.resolve([]);
     if (USE_SHAPES) {
       if (SHAPES_URL) {
-        shapesPromise = fetchDomainHttpResource(SHAPES_URL, 'SHAPES_URL')
+        shapesPromise = fetchDomainHttpResource(SHAPES_URL, "SHAPES_URL");
       }
     }
 
@@ -72,7 +90,7 @@ const fetchDomain = (features) => {
       associationsPromise,
       sourcesPromise,
       sitesPromise,
-      shapesPromise
+      shapesPromise,
     ])
       .then((response) => {
         // no magic numbers
@@ -82,35 +100,26 @@ const fetchDomain = (features) => {
           sources: response[2],
           sites: response[3],
           shapes: response[4],
-          notifications
+          notifications,
+        };
+
+        if (notifications.length > 0) {
+          const errorList = notifications.map(
+            (error) => `\n\n${error.message}`
+          );
+          throw new Error(
+            `Some URLs returned negative. If you are in development, check the server is running ${errorList}`
+          );
         }
-        // eslint-disable-next-line no-prototype-builtins
-
-        // console.log(notifications)
-        // const hasErrors = Object.values(result).some((resp) => {
-        //  // console.log(notifications)
-        //   // console.log(resp)
-        //   resp.hasOwnProperty('error')
-        // })
-
-        // if (notifications.length > 0) {
-        //   throw new Error(
-        //     'Some URLs returned negative. If you are in development, check the server is running'
-        //   )
-        // }
-        // dispatch(toggleFetchingDomain())
-        // dispatch(setInitialCategories(result.associations))
-        return result
+        return result;
       })
-      .catch((err) => {
-        console.log('error', err.message)
-
-        // dispatch(fetchError(err.message))
-        // dispatch(toggleFetchingDomain())
+      .catch((error) => {
+        console.log("error", error.message);
         // TODO: handle this appropriately in React hierarchy
-        // alert(err.message)
-      })
-  }
-}
+        // rather than alert would a panel not be better
+        // alert(error.message)
+      });
+  };
+};
 
-export default fetchDomain
+export default fetchDomain;
